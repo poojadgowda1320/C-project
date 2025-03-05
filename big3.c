8f605da1b11278753c7932d3732787d6880c25de
Ubuntu <ubuntu@ip-172-31-4-133.ap-south-1.compute.internal>
2025-03-05 06:22:15 +0000

    added c project

---
+++ big3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void  biggest3()
+{
+    int A, B, C;
+    printf("Enter the number A, B and C: ");
+    scanf("%d %d %d", &A, &B, &C);
+ 
+    if (A >=B && A >= C) 
+            printf("%d is the largest number.", A);
+        else if(B >= A && B >=C)
+            printf("%d is the largest number.", B);
+  
+        else
+            printf("%d is the largest number.", C);
+   
+
+//    return 0;
+}
